348146c8be08689ab01f02352fa37c4d8257d8ad
Rahul Budhiraja <rahul.budhiraja.dark@gmail.com>
2013-09-09 03:31:30 +0530

    implementing 2nd filter

--- src/com/example/newimagefilters/MainActivity.java
+++ src/com/example/newimagefilters/MainActivity.java
@@ -21,7 +21,9 @@ import android.view.View.OnClickListener;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity 
@@ -35,7 +37,13 @@ public class MainActivity extends Activity
 	static int index=0;
 	Button backButton,frontButton,origButton;
 	Context activityContext;
-	 boolean showOriginalImage=false;
+	boolean showOriginalImage=false;
+	 
+	ImageButton filter_backButton,filter_frontButton;
+	
+	int activeFilter=0;
+	int numFilters=2;
+	TextView filterName;
 	
 	
 	public void onCreate(Bundle savedInstanceState) 
@@ -92,7 +100,20 @@ public class MainActivity extends Activity
 		origButton.setId(125);
 		origButton.setOnClickListener(filtersLayerClickListener);
 		
+		filter_backButton=(ImageButton)findViewById(R.id.filter_backButton);
+		filter_backButton.setId(126);
+		filter_backButton.setOnClickListener(filtersLayerClickListener);
+		
+		filter_frontButton=(ImageButton)findViewById(R.id.filter_frontButton);
+		filter_frontButton.setId(127);
+		filter_frontButton.setOnClickListener(filtersLayerClickListener);
+		
+		filterName=(TextView)findViewById(R.id.filterName);
+				
 		implementVintageFilter();
+//		implementXPro2Filter();
+		
+		
 		imgView.setImageBitmap(bmpmutable);
 		
 		origButton.setText("Original");
@@ -108,9 +129,9 @@ public class MainActivity extends Activity
 			
 			if(v.getId()==123)
 				index=index+1;
-			if(v.getId()==124&&index!=0)
+			else if(v.getId()==124&&index!=0)
 				index=index-1;
-			if(v.getId()==125)
+			else if(v.getId()==125)
 			{
 				showOriginalImage=!showOriginalImage;
 				
@@ -121,6 +142,27 @@ public class MainActivity extends Activity
 				
 				
 			}
+			
+			else if(v.getId()==127)
+			{			
+				activeFilter=(activeFilter+1)%numFilters;
+				
+				
+			}
+			
+			else if(v.getId()==126)
+			{
+				
+				activeFilter=(activeFilter-1)%numFilters;
+				if(activeFilter<0)
+					activeFilter=0;
+				
+			}
+			
+			if(activeFilter==0)
+					filterName.setText("Vintage");
+			else if(activeFilter==1)
+					filterName.setText("XPro II");
 //			index=index%resIds.size();
 			
 			bmptobeManipulated=null;
@@ -131,7 +173,12 @@ public class MainActivity extends Activity
 			
 			if(!showOriginalImage)
 			{
-				implementVintageFilter();
+				if(activeFilter==0)
+				   implementVintageFilter();
+				else if(activeFilter==1)
+				   implementXPro2Filter();
+				
+				
 				imgView.setImageBitmap(null);
 				imgView.setImageBitmap(bmpmutable);
 			}
@@ -145,6 +192,112 @@ public class MainActivity extends Activity
 	{
 		super.onStart();
 	
+	}
+	
+	private void implementXPro2Filter()
+	{
+		bmpmutable=bmptobeManipulated.copy(Bitmap.Config.ARGB_8888, true);
+		bmpmutable_bgLayer=bmptobeManipulated.copy(Bitmap.Config.ARGB_8888, true);
+		bmpmutable_fgLayer=bmptobeManipulated.copy(Bitmap.Config.ARGB_8888, true);
+		
+		int rgbval[]=new int[bmpmutable.getWidth()*bmpmutable.getHeight()];
+		bmpmutable.getPixels(rgbval, 0, bmpmutable.getWidth(), 0, 0,  bmpmutable.getWidth(), bmpmutable.getHeight());
+	
+		int A, R, G, B;
+	    int pixel;
+	    
+	    double contrast_value=20;
+	    double contrast = Math.pow((100 + contrast_value) / 100, 2);
+
+	 
+		 for(int x = 0; x < bmpmutable.getWidth(); ++x) {
+		      for(int y = 0; y < bmpmutable.getHeight(); ++y) {
+		        // get pixel color
+		        pixel = bmpmutable.getPixel(x, y);
+		        // apply filtering on each channel R, G, B
+		        
+		        // Step 1 : Modifying input and output channels 
+		        A = Color.alpha(pixel);
+		        R = (int)    (((Color.red(pixel)+28) *0.77*1.09)  ) ;
+		        G = (int)((Color.green(pixel)*0.77*1.06));
+		        B = (int)((Color.blue(pixel)*0.77*1.12))+45;
+		       
+		        // Step 2: Modifying brightness and contrast ..
+		        
+		        R+=12;G+=12;B+=12;
+		        
+		        R = (int)(((((R / 255.0) - 0.5) * contrast) + 0.5) * 255.0);
+		        G = (int)(((((G / 255.0) - 0.5) * contrast) + 0.5) * 255.0);
+		        B = (int)(((((B / 255.0) - 0.5) * contrast) + 0.5) * 255.0);
+		        
+		        
+		        // Step 3: Levels again ..
+		        
+		        R=(int)((R+3)*1.03/2)       -23+11 ;
+		        G=(int)((G+3)*1.03/1.12) -5;
+		        B=(int)((B+3)*1.03*0.56) -17+15;
+		        
+		        R+=20;B+=20;G+=20;
+//		        
+//		        // Step 4: Adjust Brightness and Contrast ..
+//		        contrast_value=26;
+//		        contrast = Math.pow((100 + contrast_value) / 100, 2);
+//		        
+//		        R = (int)(((((R / 255.0) - 0.5) * contrast) + 0.5) * 255.0);
+//		        G = (int)(((((G / 255.0) - 0.5) * contrast) + 0.5) * 255.0);
+//		        B = (int)(((((B / 255.0) - 0.5) * contrast) + 0.5) * 255.0);
+//		       
+//		        
+		        
+		        // Step 5:Adjust levels again ..
+		        //    B-=14;
+		        // Step 6:Adjust levels again ..
+		           // B+=14;
+		        
+		        if(R > 255) { R = 255; }
+			              else if(R < 0) { R = 0; }
+			         
+			            
+			              if(G > 255) { G = 255; }
+			              else if(G < 0) { G = 0; }
+			         
+			             
+			              if(B > 255) { B = 255; }
+			              else if(B < 0) { B = 0; }
+			        // set new color pixel to output bitmap
+			        bmpmutable_bgLayer.setPixel(x, y, Color.argb(A, R, G, B));
+		      }
+		 }
+		 
+		 Paint p=new Paint(Paint.ANTI_ALIAS_FLAG);
+			
+			p.setARGB(255, 252,255,235);
+			p.setStyle(Style.FILL);
+			
+	        p.setFilterBitmap(false);
+	        
+	        multiplyPaint=new Paint(Paint.ANTI_ALIAS_FLAG);
+			multiplyPaint.setXfermode(new PorterDuffXfermode(Mode.MULTIPLY));
+			multiplyPaint.setFilterBitmap(false);
+	
+			Canvas fgCanvas=new Canvas(bmpmutable_fgLayer);
+
+			// set the blending mode to multiply ..
+			
+			fgCanvas.drawRect(new Rect(0,0,bmpmutable.getWidth(),bmpmutable.getHeight()), p);
+			
+			
+				
+			//fgCanvas.drawBitmap(bmptobeManipulated,0,0, multiplyPaint);
+			
+			Canvas combinedCanvas=new Canvas(bmpmutable);
+			
+			combinedCanvas.drawBitmap(bmpmutable_fgLayer,0,0,null);
+			
+			combinedCanvas.drawBitmap(bmpmutable_bgLayer,0,0,multiplyPaint);
+			
+		 
+		 
 	}
 	
 	private void implementVintageFilter() {
